d98cb926474b813a029d23d6d6d505ac2eef6e03
2pizzzza <amit.kurbanov.123.com@gmail.com>
2023-12-17 00:24:01 +0600

    learned how to work compose fun

--- app/src/main/java/com/example/composepractici/MainActivity.kt
+++ app/src/main/java/com/example/composepractici/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composepractici.ui.theme.ComposePracticiTheme
 
@@ -16,8 +18,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            newText()
         }
     }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun newText(){
+    Text(text = "Hello World",
+        color = Color.Black,
+        fontFamily = FontFamily.Monospace
+    )
 }
 
