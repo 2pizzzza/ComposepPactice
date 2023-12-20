c2d1721dfd111692ffce48d8bc392821fb73c8d5
2pizzzza <amit.kurbanov.123.com@gmail.com>
2023-12-20 19:41:11 +0600

    i dont what work this but work

--- app/src/main/java/com/example/composepractici/MainActivity.kt
+++ app/src/main/java/com/example/composepractici/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -22,13 +24,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.sp
 import com.example.composepractici.ui.theme.ComposePracticiTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            newText()
+            newText(
+
+            )
         }
     }
 }
@@ -38,16 +43,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun newText(){
     Column (
-        modifier = Modifier.
-        background(Color.LightGray)
-            .width(Dp(300f))
-            .height(Dp(200f))
-            .fillMaxHeight(100f),
+        modifier = Modifier
+            .background(Color.LightGray)
+            .fillMaxHeight()
+            .fillMaxWidth(),
 
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .background(Color.Cyan)
+        ) {
             Text(text = "Hello World",
                 color = Color.Black,
                 fontFamily = FontFamily.Monospace
@@ -68,6 +76,32 @@ fun newText(){
             )
         }
     }
+}
 
+@Preview (showBackground = true)
+@Composable
+fun novba(){
+    Row (
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxWidth()
+            .height(Dp(40f)),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "home",
+            fontSize = 20.sp
+            )
+        Text(
+            text = "Market",
+            fontSize = 20.sp
+            )
+        Text(
+            text = "Account",
+            fontSize = 20.sp,
 
+            )
+
+    }
 }
