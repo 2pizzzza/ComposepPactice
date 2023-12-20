90e527a9b073413cce2208c73e14656525e0e795
2pizzzza <amit.kurbanov.123.com@gmail.com>
2023-12-20 18:58:22 +0600

    modifier

--- app/src/main/java/com/example/composepractici/MainActivity.kt
+++ app/src/main/java/com/example/composepractici/MainActivity.kt
@@ -3,17 +3,25 @@ package com.example.composepractici
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import com.example.composepractici.ui.theme.ComposePracticiTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +37,16 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun newText(){
-    Column {
+    Column (
+        modifier = Modifier.
+        background(Color.LightGray)
+            .width(Dp(300f))
+            .height(Dp(200f))
+            .fillMaxHeight(100f),
+
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Row {
             Text(text = "Hello World",
                 color = Color.Black,
